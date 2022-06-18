db23d99301ba4ec2803a216b8d4d8535adb3a28a
sean-lgt <852951042@qq.com>
2022-06-18 15:43:53 +0800

    ✨ [nexttick]将回调推迟到下一个 DOM 更新周期之后执行

--- 09_learn_vuex/src/main.js
+++ 09_learn_vuex/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import App from './App_nexttick.vue'
 import store from './store'
 
 createApp(App).use(store).mount('#app')
